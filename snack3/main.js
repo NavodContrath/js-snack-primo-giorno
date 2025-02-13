94cd06b6a07c423af2bbc87da628e2c18d4993a9
NavodContrath <navod.contrath@gmail.com>
2025-02-13 12:08:17 +0100

    add first task snack-3

---
+++ snack3/main.js
@@ -0,0 +1,10 @@
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+let sum = 0
+for (let i = 0; i < 10; i++) {
+    const thisNumber = Number(prompt("Type here a number"))
+    console.log(thisNumber);
+    sum = sum + thisNumber;
+}
+console.log(sum);
