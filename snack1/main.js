1f454a92eb5fc287ab09cc3e1973b92954025068
NavodContrath <navod.contrath@gmail.com>
2025-02-13 10:37:07 +0100

    edit first snack-1

--- snack1/main.js
+++ snack1/main.js
@@ -1,10 +1,12 @@
 /* L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
 
-const number_1 = prompt("insert a number")
-const number_2 = prompt("insert a second number")
+const number_1 = Numberprompt("insert a number")
+const number_2 = Numberprompt("insert a second number")
 if (number_1 > number_2) {
     console.log(`the larger number is ${number_1}`);
+} else if (number_1 === number_2) {
+    console.log(`the numbers are equal`);
 } else {
     console.log(`the larger number is ${number_2}`);
 }
