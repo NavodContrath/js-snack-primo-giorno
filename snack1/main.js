9e563e7d29bbf1a06500f283ea7587e9f1f8ea2b
NavodContrath <navod.contrath@gmail.com>
2025-02-13 10:57:16 +0100

    add isNan for error numeric value

--- snack1/main.js
+++ snack1/main.js
@@ -3,6 +3,9 @@ Il software stampa il maggiore. */
 
 const number_1 = Number(prompt("insert a number"))
 const number_2 = Number(prompt("insert a second number"))
+if (isNaN(number_1) || isNaN(number_2)) {
+    alert("please insert a numeric value")
+}
 if (number_1 > number_2) {
     console.log(`the larger number is ${number_1}`);
 } else if (number_1 === number_2) {
