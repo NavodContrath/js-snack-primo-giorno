14a0352da5731acb5368754315b22ca21919053c
NavodContrath <navod.contrath@gmail.com>
2025-02-13 10:39:35 +0100

    fix prompts first snack-1

--- snack1/main.js
+++ snack1/main.js
@@ -1,8 +1,8 @@
 /* L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
 
-const number_1 = Numberprompt("insert a number")
-const number_2 = Numberprompt("insert a second number")
+const number_1 = Number(prompt("insert a number"))
+const number_2 = Number(prompt("insert a second number"))
 if (number_1 > number_2) {
     console.log(`the larger number is ${number_1}`);
 } else if (number_1 === number_2) {
